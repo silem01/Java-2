9f88029c25bf16892b9be3bfa6317c51b6e86f85
silem01 <106535395+silem01@users.noreply.github.com>
2024-10-08 14:53:24 -0300

    Clase 1 - Ejercicios

---
+++ Clase01/src/main/java/com/coderhouse/inicio/Clase01.java
@@ -0,0 +1,63 @@
+package com.coderhouse.inicio;
+
+public class Clase01 {
+
+	public static void main(String[] args) {
+	/*	//Declaración de variables
+		
+	int n1 = 10;
+	int n2 = 12;
+	int resultado = n1+n2;
+	
+	/*var numero = 1;
+	 * numero = 10; */
+	 
+	//String numeroA = "Hola";
+	
+	//String letra 'a';
+	
+	//String letraA = "b";
+	
+	//String letrab = "dsdasd";
+	
+	//Concatenacion
+	
+	//System.out.println("La suma entre " + n1 + " y "+ n2 + " es " + resultado);
+	//System.out.println("El resultado es: " + (n1 + n2) );
+
+	/*	
+		float n1 = 10;
+		float n2 = 10;
+		float resultado;
+		
+		// Calculadora
+		resultado = n1 / n2; 
+		
+		System.out.println("El resultado es: " + resultado);
+		
+		*/
+		
+		//Incrementos 
+		int contador = 1;
+		
+		System.out.println(contador);
+		contador++;
+		System.out.println(contador);
+		contador++;
+		System.out.println(contador);
+		contador++;
+		System.out.println(contador);
+		contador++;
+		System.out.println(contador);
+		contador--;
+		System.out.println(contador);
+		contador--;
+		System.out.println(contador);
+		
+		
+		
+		
+		
+	}
+
+}
